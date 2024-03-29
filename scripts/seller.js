95e4ed031adb49ccc7b243568339fbdc1e984c31
Murshed-ma1706102 <112081057+Murshed-ma1706102@users.noreply.github.com>
2024-03-29 08:36:50 +0300

    can now see the details for sold items

--- scripts/seller.js
+++ scripts/seller.js
@@ -1,17 +1,19 @@
-function getSelectedItemValue() {
-  const selectedRadio = document.querySelector('input[name="item"]:checked');
-  return selectedRadio ? selectedRadio.value : null; 
-}
 
-const currentUser= JSON.parse(localStorage.getItem("currentUser"));
-const jsonItems = localStorage.getItem("items");
-let items = jsonItems ? JSON.parse(jsonItems):[];
-
-const jsonSoldItems = localStorage.transactions
-let soldItems = jsonSoldItems ? JSON.parse(jsonSoldItems) : [];
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  // reading from localStorage
+  const currentUser= JSON.parse(localStorage.getItem("currentUser"));
+  const jsonItems = localStorage.getItem("items");
+  let items = jsonItems ? JSON.parse(jsonItems):[];
+  const jsonSoldItems = localStorage.transactions
+  let soldItems = jsonSoldItems ? JSON.parse(jsonSoldItems) : [];
+  
+    //for initial load
+  renderAvailableItem();
+  renderSoldItems();
+
+  //for filtering
   const radioButtons = document.querySelectorAll('input[name="item"]');
   radioButtons.forEach((radio) => {
     radio.addEventListener("change", () => {
@@ -19,150 +21,186 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-   //for initial load
-  renderAvailableItem();
-  renderSoldItems();
+  //for filtering
+  function getSelectedItemValue() {
+    const selectedRadio = document.querySelector('input[name="item"]:checked');
+    return selectedRadio ? selectedRadio.value : null; 
+  }
+
+  function renderItems() {
+    const cards = document.querySelector(".cards");
+    cards.innerHTML = "";
+    if (getSelectedItemValue() == "soldItems") 
+      renderSoldItems();
+    else if (getSelectedItemValue() == "itemsOnSale") 
+      renderAvailableItem();
+    else {
+      renderAvailableItem();
+      renderSoldItems();
+    }
+  }
+  
+  
+  
+  function renderAvailableItem() {
+    const container = document.querySelector(".cards");
+  
+    items.forEach((item) => {
+      if (item.sellerId === currentUser.userId) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+  
+        let img = document.createElement("img");
+        img.src = item.src;
+  
+        let desc = document.createElement("p");
+        desc.innerHTML = `description:${item.describtion}`;
+  
+        let div = document.createElement("div");
+        div.innerHTML = `<span>price: </span> <span id="price">${item.price}</span><span>$</span>`;
+  
+      
+  
+        const soldStatus = document.createElement("div");
+        soldStatus.classList.add("sold");
+        soldStatus.classList.add("on-sale");
+        const soldText = document.createElement("p")
+        soldText.innerHTML = "On Sale"
+        soldStatus.appendChild(soldText)
+  
+        const quantity = document.createElement("p")
+        quantity.innerHTML = `Stock left: ${item.quantity}`
+  
+        let btn = document.createElement("button");
+        btn.innerText = "Details";
+        btn.addEventListener("click",() => {showOnSaleDetails(item)})
+  
+        card.appendChild(soldStatus);
+        //card.appendChild(img);
+        card.appendChild(desc);
+        //card.appendChild(quantity)
+        //card.appendChild(div);
+        card.appendChild(btn)
+  
+        container.appendChild(card)
+      }
+    });
+    
+  }
+  
+   function renderSoldItems(){
+  
+      const container = document.querySelector(".cards");
+  
+      soldItems.forEach(soldItem => {
+          if (soldItem.sellerId === currentUser.userId) {
+              const card = document.createElement("div");
+              card.classList.add("card");
+  
+              let itemDesc = document.createElement("p");
+              items.forEach(item => {
+                  if(item.itemId==soldItem.itemId){
+                      itemDesc.innerHTML = item.describtion
+                  }
+              })
+  
+              const soldStatus = document.createElement("div");
+              soldStatus.classList.add("sold");
+              const soldText = document.createElement("p")
+              soldText.innerHTML = "SOLD"
+              soldStatus.appendChild(soldText) 
+  
+              const buyer = document.createElement("p");
+              buyer.innerHTML = `Buyer user id: ${soldItem.userId}`;
+              const quantity = document.createElement("p");
+              quantity.innerHTML = `Quantity: ${soldItem.quantity}`;
+              const totalPrice = document.createElement("p");
+              totalPrice.innerHTML = `Total Price: ${soldItem.totalPrice}`
+  
+              let btn = document.createElement("button");
+              btn.innerText = "Details";
+              btn.addEventListener("click",() => {
+                  showSoldDetails(soldItem)
+                  console.log("gjjgwj");
+              })
+  
+              card.appendChild(soldStatus);
+              card.appendChild(itemDesc);
+              //card.appendChild(buyer);
+             // card.appendChild(quantity);
+              //card.appendChild(totalPrice)
+              card.appendChild(btn)
+              
+              container.appendChild(card)
+      }})
+      
+  
+  }
+  
+  function showOnSaleDetails(item){
+      const cards = document.querySelector(".cards")
+      cards.innerHTML = ""
+  
+      const itemDetails = document.querySelector(".item-details")
+      
+      itemDetails.innerHTML = onSaleToHtml(item)
+      document.querySelector(".go-back").addEventListener("click", () =>{
+          itemDetails.innerHTML = ""
+          renderItems();
+      })
+  
+  }
+  
+  function showSoldDetails(item){
+      console.log("hhh");
+      const cards = document.querySelector(".cards")
+      cards.innerHTML = ""
+  
+      const soldItemDetails = document.querySelector(".item-details")
+      
+      soldItemDetails.innerHTML = soldToHtml(item)
+      document.querySelector(".go-back").addEventListener("click", () =>{
+          soldItemDetails.innerHTML = ""
+          renderItems();
+      })
+  }
+  
+  function onSaleToHtml(item) {
+  
+      
+    return `
+              
+      <img src="${item.src}">
+  
+      <div class="details">
+          <div class="sold on-sale">On Sale</div>
+          <h1>Description: ${item.describtion}</h1>
+          <h1>Quantity Left: ${item.quantity}</h1>
+          <h1>Type: ${item.type}</h1>
+          <button class="go-back">Go back</button>
+      </div>`;
+  }
+  
+  function soldToHtml(item){
+      const itemInStorage = items.find(i => i.itemId == item.itemId)
+      return `
+              
+      <img src="${itemInStorage.src}">
+  
+      <div class="details">
+          <div class="sold">SOLD</div>
+          <h1>Description: ${itemInStorage.describtion}</h1>
+          <h1>Quantity Sold: ${item.quantity}</h1>
+          <h1>Buyer ID: ${item.userId}</h1>
+          <button class="go-back">Go back</button>
+      </div>`;
+  }
 
 });
 
-function renderItems() {
-  const cards = document.querySelector(".cards");
-  cards.innerHTML = "";
-  if (getSelectedItemValue() == "soldItems") 
-    renderSoldItems();
-  else if (getSelectedItemValue() == "itemsOnSale") 
-    renderAvailableItem();
-  else {
-    renderAvailableItem();
-    renderSoldItems();
-  }
-}
 
 
 
-function renderAvailableItem() {
-  const container = document.querySelector(".cards");
-
-  items.forEach((item) => {
-    if (item.sellerId === currentUser.userId) {
-      const card = document.createElement("div");
-      card.classList.add("card");
-
-      let img = document.createElement("img");
-      img.src = item.src;
-
-      let desc = document.createElement("p");
-      desc.innerHTML = `description:${item.describtion}`;
-
-      let div = document.createElement("div");
-      div.innerHTML = `<span>price: </span> <span id="price">${item.price}</span><span>$</span>`;
-
-    
-
-      const soldStatus = document.createElement("div");
-      soldStatus.classList.add("sold");
-      soldStatus.classList.add("on-sale");
-      const soldText = document.createElement("p")
-      soldText.innerHTML = "On Sale"
-      soldStatus.appendChild(soldText)
-
-      const quantity = document.createElement("p")
-      quantity.innerHTML = `Stock left: ${item.quantity}`
-
-      let btn = document.createElement("button");
-      btn.innerText = "Details";
-      btn.addEventListener("click",() => {showOnSaleDetails(item)})
-
-      card.appendChild(soldStatus);
-      //card.appendChild(img);
-      card.appendChild(desc);
-      //card.appendChild(quantity)
-      //card.appendChild(div);
-      card.appendChild(btn)
-
-      container.appendChild(card)
-    }
-  });
-  
-}
-
- function renderSoldItems(){
-
-    const container = document.querySelector(".cards");
-
-    soldItems.forEach(soldItem => {
-        if (soldItem.sellerId === currentUser.userId) {
-            const card = document.createElement("div");
-            card.classList.add("card");
-
-            let itemDesc = document.createElement("p");
-            items.forEach(item => {
-                if(item.itemId==soldItem.itemId){
-                    itemDesc.innerHTML = item.describtion
-                }
-            })
-
-            const soldStatus = document.createElement("div");
-            soldStatus.classList.add("sold");
-            const soldText = document.createElement("p")
-            soldText.innerHTML = "SOLD"
-            soldStatus.appendChild(soldText) 
-
-            const buyer = document.createElement("p");
-            buyer.innerHTML = `Buyer user id: ${soldItem.userId}`;
-            const quantity = document.createElement("p");
-            quantity.innerHTML = `Quantity: ${soldItem.quantity}`;
-            const totalPrice = document.createElement("p");
-            totalPrice.innerHTML = `Total Price: ${soldItem.totalPrice}`
-
-            let btn = document.createElement("button");
-            btn.id = "buy";
-            btn.innerText = "Details";
-
-            card.appendChild(soldStatus);
-            card.appendChild(itemDesc);
-            //card.appendChild(buyer);
-           // card.appendChild(quantity);
-            //card.appendChild(totalPrice)
-            card.appendChild(btn)
-            
-            container.appendChild(card)
-    }})
-    
-
-}
-
-function showOnSaleDetails(item){
-    const cards = document.querySelector(".cards")
-    cards.innerHTML = ""
-
-    const itemDetails = document.querySelector(".item-details")
-    
-    itemDetails.innerHTML = onSaleToHtml(item)
-    document.querySelector(".go-back").addEventListener("click", () =>{
-        itemDetails.innerHTML = ""
-        renderItems();
-    })
-
-}
-
-function onSaleToHtml(item) {
-  return `
-            
-    <img src="${item.src}">
-
-    <div class="details">
-        <div class="sold on-sale">On Sale</div>
-        <h1>Description: ${item.describtion}</h1>
-        <h1>Quantity Left: ${item.quantity}</h1>
-        <h1>Type: ${item.type}</h1>
-        <button class="go-back">Go back</button>
-    </div>`;
-}
-
-function showSoldDetails(item){
-
-}
 
 /*
 <div class="item-details">
