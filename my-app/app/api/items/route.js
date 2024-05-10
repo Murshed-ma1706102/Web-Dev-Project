b442538eff654cda3b45ed3528894d7848f30146
AboIssa1 <ahmed.issa.202091@gmail.com>
2024-05-11 01:04:01 +0300

    edit

--- my-app/app/api/items/route.js
+++ my-app/app/api/items/route.js
@@ -3,7 +3,6 @@ import * as items from "@/repos/items"
 export async function GET(request) {
     
     try {
-        console.log(request.json().itemId)
         return Response.json( await items.get(), {status:200});
     }catch(error){
         console.log(error)
